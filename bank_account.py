8799e274638085a77b61e0cd0bd537acd831c532
raopreethim <preethi.rao.m@gmail.com>
2022-08-08 14:35:25 +0800

    committed updated the package structure

---
+++ bank_account.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+class InterestRateComputer:
+    def get_rate():
+        pass
+
+
+class BankAccount:
+    def __init__(self, balance=0, name=''):
+        self.balance = balance
+        self.name = name
+
+    def deposit(self, amount):
+        self.balance += amount
+        # recording the time of transaction for future implementation of reporting feature
+        now = datetime.now()
+        print("now =", now)
+        # dd/mm/YY H:M:S
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        print("Transaction:", amount, "-", dt_string)	
+
+
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            # recording the time of transaction for future implementation of reporting feature
+            now = datetime.now()
+            print("now =", now)
+            # dd/mm/YY H:M:S
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print("Transaction:", amount, "-", dt_string)
+        else:
+            raise Exception ("Insufficient balance!")
+
+    def interest(self, rate_computer):
+        return rate_computer.get_rate() * self.balance
+
--- src/Interest_Rate_Computer.py
+++
@@ -1,5 +0,0 @@
-
-class Interest_Rate_Computer: 
-    #To be implemented 
-    def getRate(self):
-        return None
